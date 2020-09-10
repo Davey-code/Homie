b3cb9ce51e4ec7f208ffb79dc81f65c1efa89d62
Davey_code <davis.chirao@gmail.com>
2020-09-10 12:31:06 +0300

    users devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  as :user do
+    get "signin" => 'devise/sessions#new'
+    delete "signout" => 'devise/sessions#destroy'
+    get "signup" => 'devise/registrations#new'
+  end
+
   root 'welcome#index'
 
   resources :articles
@@ -6,7 +13,7 @@ Rails.application.routes.draw do
   get 'about' => 'welcome#about'
   get 'contacts' => 'welcome#contacts'
   
-  # root 'welcome#index'
+  
   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
